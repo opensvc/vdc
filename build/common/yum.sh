7f201e3d30540182546886a8cb0c88d45b6155a3
Christophe Varoqui <christophe.varoqui@opensvc.com>
2017-09-18 16:19:16 +0200

    Add EPEL7 repo

--- build/common/yum.sh
+++ build/common/yum.sh
@@ -5,9 +5,22 @@ sudo yum-config-manager --disable CentOS\*
 sudo yum-config-manager --add-repo file:///data/repos/centos/7/base
 sudo yum-config-manager --add-repo file:///data/repos/centos/7/extras
 sudo yum-config-manager --add-repo file:///data/repos/centos/7/updates
+sudo yum-config-manager --add-repo file:///data/repos/epel/7
 sudo yum-config-manager --setopt="gpgcheck=0" --save
 
-sudo yum install -y net-tools docker bridge-utils git runc wget bind-utils python-requests mdadm sg3_utils psmisc
+sudo yum install -y \
+	net-tools \
+	docker \
+	bridge-utils \
+	git \
+	runc \
+	wget \
+        bind-utils \
+        python-requests \
+        mdadm \
+        sg3_utils \
+        psmisc \
+        htop
 
 # bugfixes
 cd /usr/libexec/docker/
