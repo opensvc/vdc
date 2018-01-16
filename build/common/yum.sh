ca78a4131e15f10399b58d22dde404f40f95c25b
Christophe Varoqui <christophe.varoqui@opensvc.com>
2018-01-16 20:04:03 +0100

    Add ElRepo for drbd feature

--- build/common/yum.sh
+++ build/common/yum.sh
@@ -6,6 +6,7 @@ sudo yum-config-manager --add-repo file:///data/repos/centos/7/base
 sudo yum-config-manager --add-repo file:///data/repos/centos/7/extras
 sudo yum-config-manager --add-repo file:///data/repos/centos/7/updates
 sudo yum-config-manager --add-repo file:///data/repos/epel/7
+sudo yum-config-manager --add-repo file:///data/repos/elrepo/7
 sudo yum-config-manager --setopt="gpgcheck=0" --save
 
 sudo yum install -y \
