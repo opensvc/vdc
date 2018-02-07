af79bef11ceb624507541535c2a526aaeb90112a
Arnaud Veron <arnaud.veron@opensvc.com>
2018-02-07 11:51:59 +0100

    Update provisioning scripts

--- build/common/yum.sh
+++ build/common/yum.sh
@@ -29,6 +29,6 @@ sudo yum install -y \
 # bugfixes
 cd /usr/libexec/docker/
 sudo ln -s docker-runc-current docker-runc
-echo "PATH=\$PATH:/usr/libexec/docker" >>/etc/sysconfig/opensvc
+echo "export PATH=\$PATH:/usr/libexec/docker" >>/etc/sysconfig/opensvc
 sed -i -e "s/: false/: true/" /etc/oci-register-machine.conf
 
