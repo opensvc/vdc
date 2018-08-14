fd0b8275acea3e9bc67b67f540b1dd965a64cb59
Arnaud Veron <arnaud.veron@opensvc.com>
2018-08-14 18:42:14 +0200

    Disable lvmetad

--- build/common/lvm.sh
+++ build/common/lvm.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+cp /etc/lvm/lvm.conf /etc/lvm/lvm.conf.preosvc
+
+cat /etc/lvm/lvm.conf.preosvc | sed -e 's/use_lvmetad = 1/use_lvmetad = 0/g' > /etc/lvm/lvm.conf
+
+rm -f /etc/lvm/lvm.conf.preosvc
+
 cat - <<EOF >>/etc/lvm/lvm.conf
 tags {
     hosttags = 1
