cc3c19fd4befb067032e4d99fd0da760fba3efce
Arnaud Veron <arnaud.veron@opensvc.com>
2018-08-15 10:52:03 +0200

    Change iscsi target address to Freenas

--- build/common/iscsi.sh
+++ build/common/iscsi.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-ISCSITGT="192.168.100.10"
+# old homemade iscsi tgt ISCSITGT="192.168.100.10"
+# Freenas
+ISCSITGT="192.168.100.210"
 
 sudo yum install -y iscsi-initiator-utils device-mapper-multipath
-
 
 cat - <<EOF >|/etc/multipath.conf
 defaults {
@@ -14,17 +15,10 @@ EOF
 
 sudo systemctl start multipathd.service
 
-
 cat - <<EOF >|/etc/iscsi/initiatorname.iscsi
 InitiatorName=iqn.2009-11.com.opensvc.srv:$HOSTNAME.storage.initiator
 EOF
 
-
 sudo iscsiadm -m discovery -t st -p $ISCSITGT
 
-for target in 1 2
-do
-    sudo iscsiadm  -m node  --targetname "iqn.2009-11.com.opensvc.srv:$HOSTNAME.storage.target.0$target" --portal "$ISCSITGT:3260" --login
-done
-
-
+sudo iscsiadm  -m node | awk '{print $2}' | xargs -n 1 sudo iscsiadm -m node --login --targetname
