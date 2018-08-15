bd4a913311d68173df6d7229af9083085cb8df35
Arnaud Veron <arnaud.veron@opensvc.com>
2018-08-15 11:59:49 +0200

    Add default repopkg to OpenSVC config

--- build/common/opensvc.sh
+++ build/common/opensvc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+osvcbranch='1.9'
+
 # user
 sudo useradd opensvc
 sudo usermod -G vagrant opensvc
@@ -8,12 +10,15 @@ echo opensvc:opensvc | sudo chpasswd
 # opensvc
 if [ -f "/etc/debian_version" ]
 then
-	dpkg -i /data/opensvc/deb/current-1.9
+	dpkg -i /data/opensvc/deb/current-${osvcbranch}
 else
-	sudo rpm -ivh /data/opensvc/rpms/current-1.9
+	sudo rpm -ivh /data/opensvc/rpms/current-${osvcbranch}
 fi
 sudo nodemgr set --param node.env --value PRD
 sudo nodemgr set --param node.dbopensvc --value https://192.168.100.9
+sudo nodemgr set --param node.repopkg --value file:///data/opensvc
+sudo nodemgr set --param node.branch --value ${osvcbranch}
+
 #sudo svcmgr -s collector create --template /data/opensvc/templates/collector.template --provision
 #sudo svcmgr -s registry create --template /data/opensvc/templates/registry.template --provision
 
