0ee90114b4a321af791b9262c4e647c2934681b7
Arnaud Veron <arnaud.veron@opensvc.com>
2019-01-09 19:23:11 +0100

    Fix nmcli network tasks
    * guess nic naming instead of hardcoding names
    * switch from delete/create to rename to avoid automatic con recreation in
    the background (ubuntu 18.04)

--- build/common/net.sh
+++ build/common/net.sh
@@ -1,39 +1,91 @@
 #!/bin/bash
 
-ETH0="eth0"
-ETH1="eth1"
-ETH2="eth2"
-ETH3="eth3"
+set -a 
+
+SLEEP=10
+
+ETH0="none"
+ETH1="none"
+ETH2="none"
+ETH3="none"
+
+# build eth list
+# eth0 : admin nic
+# eth1 : prod (encap in bridge)
+# eth2 : hb1
+# eth3 : hb2
+
+echo
+echo "# link list"
+ip -brief link show
+echo
+echo
+
+typeset -i index=0
+for nic in $(ip -brief link show | grep -Ev "^lo|^br-|^docker" | awk '{print $1}')
+do
+	eval ETH$index=$nic
+	let index=$index+1
+done
+
+for i in 0 1 2 3
+do
+	varname=ETH$i
+	echo ETH$i=${!varname}
+done
+
+# from here we have identified 4 nics defined in ETH0/1/2/3
+
+# override unmanaged-devices=* in /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf
+cat << EOF > /etc/NetworkManager/conf.d/10-globally-managed-devices.conf
+[keyfile]
+unmanaged-devices=*,except:type:wifi,except:type:wwan,except:interface-name:$ETH0,except:interface-name:$ETH1,except:interface-name:$ETH2,except:interface-name:$ETH3,except:interface-name:br-prd
+EOF
 
 if [ -f "/etc/debian_version" ]
 then
-	grep -i ubuntu /etc/lsb-release || {
-		ETH0="ens9"
-		ETH1="ens10"
-		ETH2="ens11"
-		ETH3="ens12"
-	}
 	UNIT=network-manager
-	sudo systemctl start $UNIT || /bin/true
 else
 	UNIT=network
 fi
+
+sudo systemctl -q is-active $UNIT || sudo systemctl restart $UNIT || /bin/true
+sleep $SLEEP
+
 node=$(hostname -s)
 i=$(grep $node /data/vdc.nodes | awk '{print $2}')
 
-#sudo nmcli -t --fields UUID,DEVICE c | grep -v $ETH0 | cut -d: -f1 | xargs -n1 sudo nmcli c del uuid
-sudo nmcli -t --fields UUID,DEVICE c | cut -d: -f1 | xargs -n1 sudo nmcli c del uuid
+echo
+echo "# before nmcli renaming"
+sudo nmcli c show
+echo
+
+for nic in $ETH0 $ETH1 $ETH2 $ETH3
+do
+    uuid=$(nmcli -t c show | grep $nic | awk -F':' '{print $2}')
+    nmcli c modify uuid $uuid connection.id $nic
+done
+
+echo
+echo "# after nmcli renaming"
+sudo nmcli c show
+echo
+echo
 
 sudo nmcli c add type bridge ifname br-prd con-name bridge-br-prd
 sudo nmcli c mod bridge-br-prd bridge.stp no
 sudo nmcli c mod bridge-br-prd ipv4.method manual ipv4.addresses 192.168.100.$i/24
 sudo nmcli c mod bridge-br-prd ipv4.routes "192.168.99.0/24 192.168.100.1"
-sudo nmcli c add type ethernet con-name eth0 ifname $ETH0 -- ipv4.method auto
-sudo nmcli c add type ethernet con-name eth1 ifname $ETH1 -- connection.master br-prd connection.slave-type bridge
-sudo nmcli c add type ethernet con-name eth2 ifname $ETH2 -- ipv4.method manual ipv4.addresses 192.168.101.$i/24
-sudo nmcli c add type ethernet con-name eth3 ifname $ETH3 -- ipv4.method manual ipv4.addresses 192.168.102.$i/24
+
+sudo nmcli c mod $ETH0 ipv4.method auto
+sudo nmcli c mod $ETH1 connection.master br-prd connection.slave-type bridge
+sudo nmcli c mod $ETH2 ipv4.method manual ipv4.addresses 192.168.101.$i/24
+sudo nmcli c mod $ETH3 ipv4.method manual ipv4.addresses 192.168.102.$i/24
+
+sudo nmcli c show
 
 sudo nmcli c reload
 sudo systemctl restart $UNIT
-sudo nmcli c up bridge-br-prd
+sleep $SLEEP
 
+sudo nmcli c up bridge-br-prd
