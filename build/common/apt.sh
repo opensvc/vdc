66c1c25d93cce2b3e9520f873b6bc1454115fbc6
Arnaud Veron <arnaud.veron@opensvc.com>
2019-01-09 19:22:18 +0100

    Update package list for deb based distro

--- build/common/apt.sh
+++ build/common/apt.sh
@@ -11,16 +11,24 @@ echo "source-directory /etc/network/interfaces.d" >/etc/network/interfaces
 
 sudo apt install -y \
 	network-manager \
+	dnsutils \
 	net-tools \
 	bridge-utils \
 	lvm2 \
 	git \
+	jq \
 	wget \
 	python-requests \
+	python-crypto \
 	mdadm \
+	mlocate \
 	sg3-utils \
 	psmisc \
 	iotop \
-	python-crypto \
-	htop
-
+        docker.io \
+	links \
+	runc \
+        sg3-utils \
+	htop \
+        psmisc \
+	python3 python3-requests python3-crypto
