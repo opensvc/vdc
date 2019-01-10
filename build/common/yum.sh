59e32ed44eddfb238fed7253b30d1e69cd7ca62c
Arnaud Veron <arnaud.veron@opensvc.com>
2019-01-10 09:02:27 +0100

    Add temporary nfs mount during build

--- build/common/yum.sh
+++ build/common/yum.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# temporary static nfs mount during build (can not rely on vagrant facility with virtualbox provider)
+[[ ! -d /mnt ]] && mkdir /mnt
+mount -o soft,ro,actimeo=2 ${VDCBOXIP}:${VDCBOXEXPORT} /mnt
+
 # yum
 sudo yum-config-manager --disable CentOS\*
-sudo yum-config-manager --add-repo file:///data/repos/centos/7/base
-sudo yum-config-manager --add-repo file:///data/repos/centos/7/extras
-sudo yum-config-manager --add-repo file:///data/repos/centos/7/updates
-sudo yum-config-manager --add-repo file:///data/repos/epel/7
-sudo yum-config-manager --add-repo file:///data/repos/elrepo/7
+sudo yum-config-manager --add-repo file:///mnt/repos/centos/7/base
+sudo yum-config-manager --add-repo file:///mnt/repos/centos/7/extras
+sudo yum-config-manager --add-repo file:///mnt/repos/centos/7/updates
+sudo yum-config-manager --add-repo file:///mnt/repos/epel/7
+sudo yum-config-manager --add-repo file:///mnt/repos/elrepo/7
 sudo yum-config-manager --setopt="gpgcheck=0" --save
 
 sudo yum install -y \
