521ef5350f5018c380d4176e04562b3c5a415877
Arnaud Veron <arnaud.veron@opensvc.com>
2019-01-21 10:12:52 +0100

    Add autofs configuration for shared nfs repo

---
+++ build/common/autofs.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+
+# systemctl status nfs-mountd.service
+
+cat - <<EOF >|/etc/auto.master.d/osvcdata.autofs
+${VMAUTOFSROOT}    /etc/auto.osvcdata    --ghost,--timeout=30
+EOF
+
+cat - <<EOF >|/etc/auto.osvcdata
+${VMAUTOFSKEY} -fstype=nfs,ro,soft,actimeo=2,rsize=8192,wsize=8192   ${VDCBOXIP}:${VDCBOXEXPORT}
+EOF
+
+systemctl enable autofs.service
+systemctl restart autofs.service
+
+[[ ! -L /data ]] && {
+	ln -sf ${VMAUTOFSROOT}/${VMAUTOFSKEY} /data
+}
+
+[[ -f /etc/yum.conf ]] && {
+    # switch to autofs repos
+    yum clean all
+    rm -rf /var/cache/yum/* /etc/yum.repos.d/*
+    sudo yum-config-manager -q --add-repo file:///data/repos/centos/7/base
+    sudo yum-config-manager -q --add-repo file:///data/repos/centos/7/extras
+    sudo yum-config-manager -q --add-repo file:///data/repos/centos/7/updates
+    sudo yum-config-manager -q --add-repo file:///data/repos/epel/7
+    sudo yum-config-manager -q --add-repo file:///data/repos/elrepo/7
+    sudo yum repolist
+}
