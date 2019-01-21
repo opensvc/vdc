3d3c3a9afa20398d2cb00160c97f8d22c4dfdb71
Arnaud Veron <arnaud.veron@opensvc.com>
2019-01-21 08:57:24 +0100

    Add idempotent feature to lvm.sh script

--- build/common/lvm.sh
+++ build/common/lvm.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
+grep -q 'use_lvmetad = 1' /etc/lvm/lvm.conf || {
 cp /etc/lvm/lvm.conf /etc/lvm/lvm.conf.preosvc
-
 cat /etc/lvm/lvm.conf.preosvc | sed -e 's/use_lvmetad = 1/use_lvmetad = 0/g' > /etc/lvm/lvm.conf
-
 rm -f /etc/lvm/lvm.conf.preosvc
+}
 
+grep -q 'hosttags = 1' /etc/lvm/lvm.conf || {
 cat - <<EOF >>/etc/lvm/lvm.conf
 tags {
     hosttags = 1
     local {}
 }
 EOF
+}
 
+grep -q volume_list /etc/lvm/lvm_$HOSTNAME.conf || {
 cat - <<EOF >>/etc/lvm/lvm_$HOSTNAME.conf
 activation {
     volume_list = ["@local", "@$HOSTNAME"]
 }
 EOF
+}
 
 if [ ! -f "/etc/debian_version" ]
 then
 	vgchange --addtag local VolGroup00
 fi
 
+exit 0
