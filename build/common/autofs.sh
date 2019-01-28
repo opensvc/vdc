c94e6926e3069431732fb549ae8de9707391de08
Arnaud Veron <arnaud.veron@opensvc.com>
2019-01-28 06:34:54 +0100

    Add NFS pool autofs setup

--- build/common/autofs.sh
+++ build/common/autofs.sh
@@ -12,10 +12,15 @@ grep -q '/data/vdc/share /mnt nfs' /proc/mounts && {
 
 cat - <<EOF >|/etc/auto.master.d/osvcdata.autofs
 ${VMAUTOFSROOT}    /etc/auto.osvcdata    --ghost,--timeout=30
+/nfspool   /etc/auto.nfspool     --ghost,--timeout=30
 EOF
 
 cat - <<EOF >|/etc/auto.osvcdata
 ${VMAUTOFSKEY} -fstype=nfs,ro,soft,actimeo=2,rsize=8192,wsize=8192   ${VDCBOXIP}:${VDCBOXEXPORT}
+EOF
+
+cat - <<EOF >|/etc/auto.nfspool
+data -fstype=nfs,rw,soft,actimeo=2,rsize=8192,wsize=8192   ${VDCBOXIP}:/data/vdc/share/nfspool
 EOF
 
 systemctl enable autofs.service
