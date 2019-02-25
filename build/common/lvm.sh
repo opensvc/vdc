910269d9d5ee7aa9572b6056e3b3ed8ab2fb6d4c
Arnaud Veron <arnaud.veron@opensvc.com>
2019-02-25 14:51:36 +0100

    Tag as local lvm root vg whatever the distro

--- build/common/lvm.sh
+++ build/common/lvm.sh
@@ -6,12 +6,14 @@ ROOTVG=$(lvs --noheadings -o name,vg_name | grep -w root | awk '{print $2}')
 
 
 grep -q 'use_lvmetad = 1' /etc/lvm/lvm.conf || {
+echo "Disable lvmetad"
 cp /etc/lvm/lvm.conf /etc/lvm/lvm.conf.preosvc
 cat /etc/lvm/lvm.conf.preosvc | sed -e 's/use_lvmetad = 1/use_lvmetad = 0/g' > /etc/lvm/lvm.conf
 rm -f /etc/lvm/lvm.conf.preosvc
 }
 
 grep -q 'hosttags = 1' /etc/lvm/lvm.conf || {
+echo "Enable lvm hosttags parameter"
 cat - <<EOF >>/etc/lvm/lvm.conf
 tags {
     hosttags = 1
@@ -21,6 +23,7 @@ EOF
 }
 
 grep -q volume_list /etc/lvm/lvm_$HOSTNAME.conf >> /dev/null 2>&1 || {
+echo "Configure lvm hosttags"
 cat - <<EOF >>/etc/lvm/lvm_$HOSTNAME.conf
 activation {
     volume_list = ["@local", "@$HOSTNAME"]
@@ -28,8 +31,9 @@ activation {
 EOF
 }
 
-if [ ! -f "/etc/debian_version" -a ! -z ${ROOTVG} ]
+if [ ! -z ${ROOTVG} ]
 then
+	echo "Add tag local to rootvg"
 	vgchange --addtag local ${ROOTVG}
 fi
 
