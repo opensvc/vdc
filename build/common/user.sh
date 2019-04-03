915ce4143045898ce65c7fb1ba8fd569bc82c4ff
Arnaud Veron <arnaud.veron@opensvc.com>
2019-04-03 19:26:36 +0200

    Add custom setup password for vagrant user
    * Generate base64 hash string with command below
    	mkpasswd --method=sha-512 --salt=yoursalt | base64
    
    * Fill in the string in the global vagrant config file

--- build/common/user.sh
+++ build/common/user.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+env | grep -q VAGRANTPASSWDBASE64HASH || {
+	echo
+	echo "Error : VAGRANTPASSWDBASE64HASH env variable is expected to be set. Exiting"
+	echo
+	exit 1
+}
+
 # user
 getent passwd opensvc || {
 	sudo useradd opensvc
@@ -8,4 +15,9 @@ getent passwd opensvc || {
 
 getent group vagrant | grep -q opensvc || {
 	sudo usermod -G vagrant opensvc
+}
+
+getent passwd vagrant && {
+	echo "Changing password for user vagrant"
+        echo "vagrant:$(echo ${VAGRANTPASSWDBASE64HASH} | base64 -d)" | sudo chpasswd --encrypted
 }
