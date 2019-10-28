aebd9e404bfece05b8080e99b2c80eb4eb43db4b
Arnaud Veron <arnaud.veron@opensvc.com>
2019-10-28 06:00:04 +0100

    Add osvc env variables

---
+++ build/common/setenv.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo
+echo "########################"
+echo "######## SETENV ########"
+echo "########################"
+echo
+echo
+
+ENVFILE=/etc/profile.d/sh.local
+
+CLUSTER_NUMBER=$(grep -w ^$HOSTNAME /vagrant/vdc.nodes | sort -u | awk '{print $2}')
+
+grep -q "export OSVC_CLUSTER_NUMBER=${CLUSTER_NUMBER}" ${ENVFILE} || {
+echo "Adding OSVC_CLUSTER_NUMBER=${CLUSTER_NUMBER} to ${ENVFILE}"
+cat - <<EOF >> ${ENVFILE}
+export OSVC_CLUSTER_NUMBER=${CLUSTER_NUMBER}
+EOF
+
+}
+
+grep -q "export OSVC_CLUSTER_NAME=cluster${CLUSTER_NUMBER}" ${ENVFILE} || {
+echo "Adding OSVC_CLUSTER_NAME=cluster${CLUSTER_NUMBER} to ${ENVFILE}"
+cat - <<EOF >> ${ENVFILE}
+export OSVC_CLUSTER_NAME=cluster${CLUSTER_NUMBER}
+EOF
+
+}
+
+grep -q "export OSVC_SVC1_NAME=c${CLUSTER_NUMBER}svc1" ${ENVFILE} || {
+echo "Adding OSVC_SVC?_NAME=c${CLUSTER_NUMBER}? to ${ENVFILE}"
+cat - <<EOF >> ${ENVFILE}
+export OSVC_SVC1_NAME=c${CLUSTER_NUMBER}svc1
+export OSVC_SVC2_NAME=c${CLUSTER_NUMBER}svc2
+export OSVC_SVC3_NAME=c${CLUSTER_NUMBER}svc3
+export OSVC_SVC4_NAME=c${CLUSTER_NUMBER}svc4
+export OSVC_SVC5_NAME=c${CLUSTER_NUMBER}svc5
+export OSVC_SVC6_NAME=c${CLUSTER_NUMBER}svc6
+export OSVC_SVC7_NAME=c${CLUSTER_NUMBER}svc7
+export OSVC_SVC8_NAME=c${CLUSTER_NUMBER}svc8
+export OSVC_SVC9_NAME=c${CLUSTER_NUMBER}svc9
+EOF
+
+}
+
+exit 0
