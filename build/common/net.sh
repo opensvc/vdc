92609b40d6c405b3b388adb4ac5ffc932c2540f3
Arnaud Veron <arnaud.veron@opensvc.com>
2020-04-21 09:27:01 +0200

    Remove dependency to hardcoded nic names
    * now rely on virtualbox showvminfo that must exist in /vagrant

--- build/common/net.sh
+++ build/common/net.sh
@@ -14,10 +14,10 @@ set -a
 
 SLEEP=10
 
-ETH0="none"
-ETH1="none"
-ETH2="none"
-ETH3="none"
+NIC0="none"
+NIC1="none"
+NIC2="none"
+NIC3="none"
 
 # build eth list
 # eth0 : admin nic
@@ -25,42 +25,80 @@ ETH3="none"
 # eth2 : hb1
 # eth3 : hb2
 
+function mac2nic()
+{
+    local MAC=$1
+    ip -brief link show | grep -Ev "^docker|^br-|^lxc|^lo" | sed -e 's/://g' | awk '{print $1 " " toupper($3)}' | grep -w ${MAC} | awk '{print $1}'
+}
+
 echo
 echo "# link list"
 ip -brief link show
 echo
 echo
 
-typeset -i index=0
-for nic in $(ip -brief link show | grep -Ev "^lo|^br-|^docker" | awk '{print $1}')
-do
-	eval ETH$index=$nic
-	let index=$index+1
-done
+node=$(hostname -s)
+ip=$(grep -w ^$node /vagrant/vdc.nodes | sort -u | awk '{print $3}')
+cluid=$(grep -w ^$node /vagrant/vdc.nodes | sort -u | awk '{print $2}')
 
+#i=$(getent -s dns hosts ${node}|awk '{print $1}'|awk -F'.' '{print $4}'|sort -u)
+echo
+echo "found ip index <$ip>"
+echo "found cluster id <$cluid>"
+echo
+
+[[ ! -f /vagrant/$HOSTNAME.showvminfo ]] && {
+	echo "Error: missing mandatory /vagrant/$HOSTNAME.showvminfo"
+	exit 1
+}
+
+echo "Identifying mac addr from vagrant showvminfo"
+MAC0=$(grep "^NIC.*Attachment: NAT," /vagrant/${node}.showvminfo | awk -F, '{print $1}' | awk '{print $4}')
+MAC1=$(grep "^NIC.*Attachment: Bridged Interface.*br-${cluid}-0" /vagrant/${node}.showvminfo | awk -F, '{print $1}' | awk '{print $4}')
+MAC2=$(grep "^NIC.*Attachment: Bridged Interface.*br-${cluid}-1" /vagrant/${node}.showvminfo | awk -F, '{print $1}' | awk '{print $4}')
+MAC3=$(grep "^NIC.*Attachment: Bridged Interface.*br-${cluid}-2" /vagrant/${node}.showvminfo | awk -F, '{print $1}' | awk '{print $4}')
+
+echo "Resolving ip link name from mac addr"
 for i in 0 1 2 3
 do
-	varname=ETH$i
-	echo ETH$i=${!varname}
+	macname=MAC$i
+	nic=`mac2nic ${!macname}`
+	eval NIC$i=$nic
 done
+
+echo "Nic mapping"
+for i in 0 1 2 3
+do
+	nicname=NIC$i
+	macname=MAC$i
+	echo NIC$i=${!nicname}    MAC$i=${!macname}
+done
+
+cat << EOF > /etc/profile.d/osvc.nic.sh
+export OSVC_NIC0=${NIC0}
+export OSVC_NIC1=${NIC1}
+export OSVC_NIC2=${NIC2}
+export OSVC_NIC3=${NIC3}
+EOF
+
+
+# from here we have identified 4 nics defined in NIC0/1/2/3
 
 if [ -f "/etc/debian_version" ]
 then
 for i in 1 2 3
 do
-	varname=ETH$i
-	# needed for nmcli debian, to list device associated to connections
-	ip link set ${!varname} down
-	ip link set ${!varname} up
+        varname=NIC$i
+        # needed for nmcli debian, to list device associated to connections
+        ip link set ${!varname} down
+        ip link set ${!varname} up
 done
 fi
-
-# from here we have identified 4 nics defined in ETH0/1/2/3
 
 # override unmanaged-devices=* in /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf
 cat << EOF > /etc/NetworkManager/conf.d/10-globally-managed-devices.conf
 [keyfile]
-unmanaged-devices=*,except:type:wifi,except:type:wwan,except:interface-name:$ETH0,except:interface-name:$ETH1,except:interface-name:$ETH2,except:interface-name:$ETH3,except:interface-name:br-prd
+unmanaged-devices=*,except:type:wifi,except:type:wwan,except:interface-name:$NIC0,except:interface-name:$NIC1,except:interface-name:$NIC2,except:interface-name:$NIC3,except:interface-name:br-prd
 EOF
 
 if [ -f "/etc/debian_version" ]
@@ -73,21 +111,12 @@ fi
 sudo systemctl restart $UNIT || /bin/true
 sleep $SLEEP
 
-node=$(hostname -s)
-ip=$(grep -w ^$node /vagrant/vdc.nodes | sort -u | awk '{print $3}')
-cluid=$(grep -w ^$node /vagrant/vdc.nodes | sort -u | awk '{print $2}')
-
-#i=$(getent -s dns hosts ${node}|awk '{print $1}'|awk -F'.' '{print $4}'|sort -u)
-echo
-echo "found ip index <$ip>"
-echo "found cluster id <$cluid>"
-echo
 echo
 echo "# before nmcli renaming"
 sudo nmcli c show
 echo
 
-for nic in $ETH0 $ETH1 $ETH2 $ETH3
+for nic in $NIC0 $NIC1 $NIC2 $NIC3
 do
     uuid=$(sudo nmcli -t --fields name,uuid,device c show | grep $nic | awk -F':' '{print $2}')
     sudo nmcli c modify uuid $uuid connection.id $nic 2>/dev/null || sudo nmcli c add type ethernet ifname $nic con-name $nic autoconnect yes save yes
@@ -108,10 +137,12 @@ sudo nmcli c mod $bridge ipv4.method manual ipv4.addresses ${VDC_SUBNET_A}.${clu
 sudo nmcli c mod $bridge ipv4.routes "192.168.99.0/24 ${VDC_SUBNET_A}.${cluid}.0.1"
 sudo nmcli c mod $bridge +ipv4.routes "192.168.200.0/24 ${VDC_SUBNET_A}.${cluid}.0.1"
 
-sudo nmcli c mod $ETH0 ipv4.method auto
-sudo nmcli c mod $ETH1 connection.master br-prd connection.slave-type bridge
-sudo nmcli c mod $ETH2 ipv4.method manual ipv4.addresses ${VDC_SUBNET_A}.${cluid}.1.${ip}/24
-sudo nmcli c mod $ETH3 ipv4.method manual ipv4.addresses ${VDC_SUBNET_A}.${cluid}.2.${ip}/24
+sudo nmcli c mod $NIC0 ipv4.method auto
+sudo nmcli c mod $NIC1 connection.master br-prd connection.slave-type bridge
+sudo nmcli c mod $NIC2 ipv4.method manual ipv4.addresses ${VDC_SUBNET_A}.${cluid}.1.${ip}/24
+sudo nmcli c down $NIC2 ; sudo nmcli c up $NIC2
+sudo nmcli c mod $NIC3 ipv4.method manual ipv4.addresses ${VDC_SUBNET_A}.${cluid}.2.${ip}/24
+sudo nmcli c down $NIC3 ; sudo nmcli c up $NIC3
 
 sudo nmcli c show
 
