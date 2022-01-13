5d8fcb2e1a217b60a237986026db2737eb043181
Arnaud Veron <arnaud.veron@opensvc.com>
2022-01-13 11:11:17 +0100

    Populate lxc dir from tarball

--- build/common/lxc.sh
+++ build/common/lxc.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+set -a
+
 echo
 echo "#####################"
 echo "######## LXC ########"
 echo "#####################"
 echo
 
+[[ -f ~vagrant/opensvc-qa.sh ]] && . ~vagrant/opensvc-qa.sh
 
 [[ -f /etc/redhat-release ]] && {
     sudo yum -y install debootstrap
@@ -19,8 +22,23 @@ LXC_REPO=/nfspool/data/lxc
 SRC_LXC_CACHE=${LXC_REPO}/cache
 DST_LXC_CACHE=/var/cache/lxc
 
-echo "Populating LXC cache from ${SRC_LXC_CACHE}"
-rsync -a ${SRC_LXC_CACHE}/ ${DST_LXC_CACHE}
+cd ${LXC_REPO} && {
+echo "Populating LXC cache from ${LXC_REPO}"
+#rsync -HAXpogDtrlx ${SRC_LXC_CACHE}/ ${DST_LXC_CACHE}
+
+test -d ${DST_LXC_CACHE} || mkdir -p ${DST_LXC_CACHE}
+
+for file in $(ls -1 *.tar)
+do
+    echo "=>    extracting archive ${file}"
+    tar xf ${file} -C ${DST_LXC_CACHE}
+done
+
+echo
+cd ${DST_LXC_CACHE} && ls -l
+echo
+}
+
 
 test -f /usr/share/lxc/templates/lxc-ubuntuosvc || {
 	test -d /usr/share/lxc/templates || mkdir -p /usr/share/lxc/templates
