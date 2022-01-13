7d3bafde7ebdde603dd5543279cf3b1397db8aa6
Arnaud Veron <arnaud.veron@opensvc.com>
2022-01-13 11:15:34 +0100

    Improve ssh config script

--- build/common/opensvc.ssh.key.sh
+++ build/common/opensvc.ssh.key.sh
@@ -6,58 +6,55 @@ echo "######## OPENSVC SSH KEYS ########"
 echo "##################################"
 echo
 
-[[ ! -d /root/.ssh ]] && mkdir /root/.ssh && touch /root/.ssh/authorized_keys
+[[ -f ~vagrant/opensvc-qa.sh ]] && . ~vagrant/opensvc-qa.sh
 
-grep -q 'opensvc-qabot' /root/.ssh/authorized_keys || {
-
-cat - <<EOF >>/root/.ssh/authorized_keys
+SSH_PUB_KEYS="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCybpHV5+7L2MShASp8w8DEqYSSj2rQaJrJNre4kGf8qA8kRUIo7k1dorwieP21mEOxPMJDBkLvS2xRhSirpZ9Ot22+/5ibl+rCNk14kCtfITxdBwEKbL9kmgxSuAAFGwpFtWVed9eEe7sFZkfBeUBljw6bDn6WdfAVhUwpqr5bbnd7aX3gV7UqvAjjsDBZtNViDdxF5W7phZQXEVAyfEnu5uSSGMX+YP+zFNNUxn5LDzoITmJHeCYYpFDSeD+bpaXC2iSBzzltfUv6VDNr8C85LMcheEsme9zFPqmRB1/juIxiSfwNUDmTlqGwM8Cx8ITRtDbvlMhLLDn95z/4HKNuEUrPCZGzL1dIvCs9V0ke7FvZnASMMNTNl4H+3dg3zPunarEI73fuGqsoaj2+2z5WZj3xpVSrXPN39whJ8a6ZD4O/3t/atiSR9EHk1NZp/TqbH8DDoRQ+xFUNdXugAOpKqk74DkE2Xazs26ifIAx8KdpSxnhd1/F7Hzgwnsc9Qpukib9UyY2DRngojE9xlYIxoMqYOQrI5vJv5JQeVefPn4T6Zmgc1IQauMNHi9Pbk99ztlEaTGusCUly9twZSgskWptv8IrYIdHZisdbHYwNJIGMEAYqLBsLQkBkJFG215L3iksH6mglLzbUZJK7d2FzuL+SUcCAJG8BxV7IkVQF6w== root@malabar
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDVLQNm56PbIIufdN80IrmuLUvQSrwD1pyHPtHC9DS2D6YQnGmC0QxiheSpWjTNFXJiaq+ba6pQAEAvySGs9BoQuk+X4TuBwbaw3HwYuStnTAUIm2wBNJmtMGyzYvvSPwYUF2t5uSgEzfNKvSMINAfLsxGH1NBDEi/yFhznc/xcRb36sSPHQA0q1/IrQ3aZoh5ugpoVdkpIbixFrsXhKmu5Ij16MzJSwC2aiyf9C1zOviwY37rfOHhTAMTAQ3VXvuAve/6pVWSKNYotxbgL0yEGk1Sdu25RO7rdJtEjYMskLXN1T2SDmh+uVyplyb8/MIvU5nLthsMuFKNuLvxE83ZH root@pez
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCbh065DQH64jyu2/8btyvmAcDZDdTPkcMnfF/CB1QQAWF+mfeq2Hb3xWEBAO5RIF1Yb2JJYIhJt8rv4zhpu8qkK9GkV5lhR68Ma1/ZXHCufYRAt6SfQZqW79hJGVDXC/tuDf+b+TNsZYPJrxFZZXkTszj4q7A3P4LGrdjnpbLL83DBaOIEKlwKBBUMLSMdi4s3G0rZJFDoeDJf3ng8TNT4N6wJidEHk7CMJZr8LIrMXLoUiZUylrFLYrOixZLnT8Az4Nf/GiHj/bDPKyFhqISQX70MSmcQ3Nsd3cGSbxAGXuy995+92qLxls6Ku1guTbFqYN8ev1XvGaxvzRz6/HeH opensvc-qabot
-EOF
-
-}
-
-grep -q 'OPENSVC-CVI' /root/.ssh/authorized_keys || {
-
-cat - <<EOF >>/root/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDPiTjBH9tZI59Y5rE/6P90ATF/RT4LU53oBcViBVi/P1tyfCMex5lTuiwOpPEgA6Rvccj7PDQVDzP6IJXBlUgWuVezLMKJoYLVlZahvMTei1l1x9a6nc0eusEfDUYg/+CzTpRBptKyCc9suUZT8AlnSbsxS1TXxe30RAfpT7eSlPw2squGUM3Unaq/dL0JKMBptUMSas8k2b+rdcZeUNPZRhgaovo2AdBoEZi4nHyLJB37mi4Eevu+sAtIcWpUvEicOaH4yKp1n4EXddO19SdLcpOZ59UnJkwzJQjq6kaAMJ8Oyivpd5QTNmmZ86aiZeZP7bBlQLHFovjbYvfEJ0BSJqZoKhbS0kRSt+Bq4Vs54GTGCIBx+pTg7l30qOdgO9fvbGRRnH/Uh/G1ba9Mz611K3mdWcW5vhASX/Hc0bpl+1LuhnQp8erpQq9kXQ6eLlHSWmXfFZwlOLb5Sshy9ov8SqK7wa4htJOS+mpq2OLJ9vLQdI1mq0s6OgQbI+DKlh9bLYqhx1qrqw83wuweAYu14Lhb40JOEpjCxrBix7yvrONpF8H0hn0Sp4dwgrQaY3wbXW62t69iegsQ/MgQrT9gdt4Yif3yzKvu10QGYwFIHDZBEkaif82Z/zQiMQPMpUzLPfci3p0Eew+pB+pglhkHOxGiCV9abcZDAO8o6mBHlw== OPENSVC-CVI
-EOF
-
-}
-
-grep -q 'OPENSVC-AVN' /root/.ssh/authorized_keys || {
-
-cat - <<EOF >>/root/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAgEAt/dDaRZwk4ggjx++FKyDLVz2VkWyoeEQ6IU2oMZsnY1BOK1RNpQi6Pb2I35yN1WWPTxY+3UtfVHJLJ08IIxEeFV8dikGjxsHz0kMSILX/SyJ7XfEl9N9bvOER3bldGDc8WIjUhWJCneCSqQhndqLELL63um0DPny/Bm3vWsFHAr7z9U2evpTLvGlrCMxKLxF70pXLwPZ+BlOFZtyHjGqe6COlDNe0UZdzIvol+KJNM4SEjtWon/BGO8MhBrG05bJbB8nG3UjOmEpQvxa6DqiF7/l5ro/M03hhJkQs7GVbhbMef0mjI2gjZSQES6NC3TTdtNBdjrKGrciA54jp9+YfP7y8AcbtKU+8WwcK2Tt5R/+krzD3rZ5E3ys6Hs023Xej+IQlddSj/qlHRYWhze5iWE3jlliTo9PTH84vToo47lcJob2dHOKIunl7An9aqQq0U3j8JNksJUP4HGMG0X9l13vByhIZ8jBRYKnr6t01HSVMTpVq/5mVeH3TG1HFZOZ0kUVs7C9XVr7auwWDaeHKbWKp3KTBpDcg//NgLL6qQkbtgkPtjyXOVRCyxsqktU5T98v1DWBFefMQdKgSM5mh/7ZVKCO03f6OgMFhntfdwOB1LCaxN4Urg6wIHT4yvIvIi5wpo0Iu/pNT+2CJLQcVC4CMsucBe6Z4r+eJn7RUR8= OPENSVC-AVN
-EOF
-
-}
-
-grep -q 'OPENSVC-GMT' /root/.ssh/authorized_keys || {
-
-cat - <<EOF >>/root/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDGCY93/GPl3pHwfma3/ikFB5pcyeiBif+LwuYA3dm2qXxzdXP3CM/P1mdfEw2JvcGuKAHI3gSaW8fm+w5T1BFF7sT8/Wj/jj2xV2S9VAadwCjVCUKrDG0AatQlE7bmvjeloCOyDxeFiiulNO/a4x5gxif7VcpMzd8emNC7viOxjaroIi4qQxs4NOx4UvlBxGWP480BV2kvAJdzU/WYJzmVdZwb1q7gyrcfNCWWl5y7g+K5qttrNPV8zluoNVMIpvujfMf6ssauaLQcP9qzifvrkx4jWus2qb6GjMiKXrZNQUuJlzxxlDlNhZiwhInPp52CoSzqSE/tBsTRF6iID8F3 OPENSVC-GMT
-EOF
-
-}
-
-grep -q 'fCa6h OPENSVC-SGF' /root/.ssh/authorized_keys || {
-
-cat - <<EOF >>/root/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzMv/8c3e0WG7swC1z7WrxVa6NXipiI3yXFc9OFKpXAwgdnULbP0y0z+2w8sXgr7aEJ2pRFkAS+LDS5XKhNvNsL/vTsU96tiI+6o7IMXJUSdWlSR3nb08Ah/RpnfZGvJV3U61vswuVv/s0h9FokktTjK6DtOp4RzsUg6WkRWVPDuf5ME6ESFIx1IQhf73LUVjktyvfzN+rDFtVZwePmoVdIRRkh6gSGXXBixbX8Ff+0VOvqPPMQuAXUaXrTAFEs2dgv0+8SYEUJ15e9duJAiSo+U4XG9WBiA9kSEBk1b5BpE+ksRZpKvzHz7vUE5vZoB7HX1EdaaI7edPGFOpfCa6h OPENSVC-SGF
-EOF
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDe9ZiqDLEDBjTTlBTf00qvbE9i1ZyvVHAvpOPfwFR7qASEgFQPfwz30rurpYfzXLAho3AvAGLB3ztQcX0FOsPJxT8c9NpS7F4e6JkH8E1q0JjUVJkhuHliRXDcQ+/kHRtmjnPKWyf8xh53PShI1x+ZSec9Ogbv5q+ac9KA+mk61Rp43Pqa4YGleKbudjgLQ/JN+kNnyW3nqQmD1m/nCjh1Lg2QwglJ6asnc57gHeFmJUBSS5oKbGnv2SBJP379jLg0SzlDTkqQIUWE89f73dMcoTlbyV1NrIpkb/uuDbiuZMbwZjo81/u7uHKzrZWI4akXu2tsT2+ptwE0HVeml1AooT09a8/DUhTdCqjVI1J1yIY9Q8HKj+nLCBrwsQzbgQHsS2/rXnMHYrQFVuGNrmBK5l8IdR5XQ9YSNlZE9Kg3lG/cbHUsg+t4E4skbcyOnqHh0Ao15kHaimBmVS0vYyEzzMFG4UWLU3mPOsl1QC9EmVm2qWuYWgDDtt6uR5gB968= ansible@avn
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKTEoNNf0l/W3ItpaOkEjssrNU1KGkGyJ3WCo+JnPm5i OPENSVC-CGN"
 
+function update_key {
+  local user="$1"
+  local public_key="$2"
+  local user_home_dir
+  local user_ssh_dir
+  local user_ssh_authorized_keys
+  [ -z "$user" -o -z "$public_key" ] && echo "invalid params: user=$user, public_key=$public_key" && return 1
+  user_home_dir=$(getent passwd $user | awk -F: '{print $6}')
+  if [ -z "$user_home_dir" ] ;then
+     echo "unable to detect user $user home dir"
+     return 1
+  fi
+  user_ssh_dir=$user_home_dir/.ssh
+  user_ssh_authorized_keys=$user_home_dir/.ssh/authorized_keys
+  if [ ! -d "$user_ssh_dir" ] ; then
+    mkdir echo $user_ssh_dir && chown $user: $user_ssh_dir && chmod og-rwx $user_ssh_dir || return 1
+    touch $user_ssh_authorized_keys && chown $user: $user_ssh_authorized_keys && chmod og-rwx $user_ssh_authorized_keys || return 1
+  fi
+  grep "$public_key" $user_ssh_authorized_keys > /dev/null && return 0
+  echo "adding new key for user $user ${public_key}"
+  echo "$public_key" >> $user_ssh_authorized_keys
+  return 0
 }
 
-grep -q 'JnPm5i OPENSVC-CGN' /root/.ssh/authorized_keys || {
-
-cat - <<EOF >>/root/.ssh/authorized_keys
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKTEoNNf0l/W3ItpaOkEjssrNU1KGkGyJ3WCo+JnPm5i OPENSVC-CGN
-EOF
-
+function update_keys {
+  echo "$SSH_PUB_KEYS" | while read ssh_key
+  do
+    update_key root "$ssh_key" || return 1
+    update_key vagrant "$ssh_key" || return 1
+  done
 }
 
-chmod 600 /root/.ssh/authorized_keys
+update_keys || exit 1
 
 [[ ! -f /root/.ssh/authorized_keys.install ]] && {
 	cp /root/.ssh/authorized_keys /root/.ssh/authorized_keys.install
 }
+
+exit 0
+
