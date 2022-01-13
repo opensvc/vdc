19dc4880bb4b497d45a34fd5d8f6776da390d2cd
Arnaud Veron <arnaud.veron@opensvc.com>
2022-01-13 11:10:21 +0100

    Switch to DRBD9 version

--- build/common/drbd.sh
+++ build/common/drbd.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 
-set -a 
+echo
+echo "######################"
+echo "######## DRBD ########"
+echo "######################"
+echo
 
-DRBD_PACKAGES="drbd84-utils kmod-drbd84"
+[[ -f ~vagrant/opensvc-qa.sh ]] && . ~vagrant/opensvc-qa.sh
+
+set -a
+
+[[ -f /etc/debian_version ]] && {
+    DEBIAN_FRONTEND=noninteractive add-apt-repository ppa:linbit/linbit-drbd9-stack
+    DEBIAN_FRONTEND=noninteractive apt-get update
+    DEBIAN_FRONTEND=noninteractive apt --no-install-recommends -y install linux-headers-$(uname -r) drbd-dkms drbd-utils
+}
+
+[[ -f /etc/redhat-release ]] && {
+    DRBD_PACKAGES="drbd90-utils kmod-drbd90"
+    yum -y install ${DRBD_PACKAGES}
+}
+
+grep -Eq "SLES|SUSE|Leap" /etc/os-release && {
+    zypper --gpg-auto-import-keys -n --non-interactive install drbd-utils drbd-kmp-default
+}
+
+modprobe drbd || exit 1
+modinfo drbd | grep ^version
+
 DRBDTOP_BIN=/data/opensvc/bin/drbdtop
 
-yum -y install ${DRBD_PACKAGES}
-
 [[ ! -f /usr/local/bin/drbdtop ]] && {
-    sudo cp $DRBDTOP_BIN /usr/local/bin/drbdtop
-    sudo chmod a+x /usr/local/bin/drbdtop
+    sudo cp $DRBDTOP_BIN /usr/bin/drbdtop
+    sudo chmod a+x /usr/bin/drbdtop
 }
+
+exit 0
