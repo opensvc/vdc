6849e58e844c2ef8e57294536c92d9433881912e
Arnaud Veron <arnaud.veron@opensvc.com>
2022-01-13 11:16:08 +0100

    Load QA variables

--- build/common/selinux.sh
+++ build/common/selinux.sh
@@ -6,6 +6,8 @@ echo "######## SELINUX ########"
 echo "#########################"
 echo
 
+[[ -f ~vagrant/opensvc-qa.sh ]] && . ~vagrant/opensvc-qa.sh
+
 [[ -f /etc/sysconfig/selinux ]] && {
 	sudo setenforce 0
 	sudo sed -i -e "s/^SELINUX=enforcing/SELINUX=disabled/" /etc/sysconfig/selinux
