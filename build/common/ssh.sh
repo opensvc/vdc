ccd66e3e41190275f86e30679f5d03136c5f7b1b
Arnaud Veron <arnaud.veron@opensvc.com>
2022-01-13 11:17:39 +0100

    Improve ssh daemon config script

--- build/common/ssh.sh
+++ build/common/ssh.sh
@@ -6,10 +6,18 @@ echo "######## SSH ########"
 echo "#####################"
 echo
 
+[[ -f ~vagrant/opensvc-qa.sh ]] && . ~vagrant/opensvc-qa.sh
+
 echo "Customizing sshd config"
 sudo sed -i -e "s/^PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config 
 sudo sed -i -e "s/^GSSAPIAuthentication yes/GSSAPIAuthentication no/" /etc/ssh/sshd_config 
+sudo sed -i -e "s/^PermitRootLogin no/PermitRootLogin yes/" /etc/ssh/sshd_config
 
 echo "Restarting sshd systemd unit"
-sudo systemctl restart sshd
+type systemctl >> /dev/null 2>&1 && sudo systemctl restart sshd
 
+grep -q FreeBSD /etc/os-release && {
+    sudo service sshd restart
+}
+
+exit 0
