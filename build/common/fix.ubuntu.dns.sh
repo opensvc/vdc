1090e489269d980bb9a5daace76cc00832a42b84
Arnaud Veron <arnaud.veron@opensvc.com>
2022-01-13 11:14:43 +0100

    Fix dns config script

--- build/common/fix.ubuntu.dns.sh
+++ build/common/fix.ubuntu.dns.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+echo
+echo "#########################"
+echo "######## FIX DNS ########"
+echo "#########################"
+echo
+
+echo
+echo "# before dns config"
+sudo nmcli c show
+echo
+echo
+
 echo "Switch Netplan to NetworkManager"
 rm -f /etc/netplan/*
 cat >| /etc/netplan/01-network-manager-all.yaml <<EOF
@@ -8,30 +20,55 @@ network:
   renderer: NetworkManager
 EOF
 sudo netplan generate
+sudo netplan apply
 
-echo "Disable DNS management through NetworkManager"
-echo -e "[main]\ndns=none" > /etc/NetworkManager/conf.d/no-dns.conf
-sudo systemctl restart NetworkManager
+echo "Setup Network Manager to configure resolver"
+cat >| /etc/NetworkManager/conf.d/10-osvc-dns.conf <<EOF
+[main]
+dns=default
+EOF
 
-echo "Disable systemd-networkd"
-sudo systemctl disable systemd-networkd
-sudo systemctl mask systemd-networkd
-sudo systemctl stop systemd-networkd
+echo "Remove stub symlink /etc/resolv.conf => ../run/systemd/resolve/stub-resolv.conf"
+rm -f /etc/resolv.conf
 
-echo "Disable systemd-resolved"
-sudo systemctl stop systemd-resolved.service
-sudo systemctl disable systemd-resolved.service
+echo "Restart Network Manager"
+systemctl restart NetworkManager
+sleep 5
+
+echo "Display /etc/resolv.conf contents"
+cat /etc/resolv.conf
+
+# load OSVC_ env variables
+#. /etc/profile
+#sudo nmcli con mod bridge-br-prd ipv4.dns 10.0.2.3
+#sudo nmcli con mod bridge-br-prd ipv4.dns-search "vdc.opensvc.com opensvc.com"
+#sudo nmcli c up bridge-br-prd
+#sudo nmcli con mod ${OSVC_NIC0} ipv4.ignore-auto-dns true
+#sudo nmcli c up ${OSVC_NIC0}
+
+#echo "Disable DNS management through NetworkManager"
+#echo -e "[main]\ndns=none" > /etc/NetworkManager/conf.d/no-dns.conf
+#sudo systemctl restart NetworkManager
+
+#echo "Disable systemd-networkd"
+#sudo systemctl disable systemd-networkd
+#sudo systemctl mask systemd-networkd
+#sudo systemctl stop systemd-networkd
+
+#echo "Disable systemd-resolved"
+#sudo systemctl stop systemd-resolved.service
+#sudo systemctl disable systemd-resolved.service
 
 echo "Cleaning /etc/hosts"
 cat /etc/hosts | grep -v `hostname -s` > /etc/hosts.clean
 cp /etc/hosts.clean /etc/hosts
 
-echo "Building static /etc/resolv.conf"
-rm -f /etc/resolv.conf
-cat >| /etc/resolv.conf <<EOF
-nameserver 10.0.2.3
-options edns0
-EOF
+#echo "Building static /etc/resolv.conf"
+#rm -f /etc/resolv.conf
+#cat >| /etc/resolv.conf <<EOF
+#nameserver 10.0.2.3
+#options edns0
+#EOF
 
 #sudo systemctl stop systemd-networkd
 #sudo systemctl disable systemd-networkd
@@ -46,3 +83,9 @@ EOF
 #sudo echo nameserver 192.168.121.1 | tee /etc/resolvconf/resolv.conf.d/tail
 #sudo ln -sf /run/resolvconf/resolv.conf /etc/resolv.conf
 #resolvconf -u
+
+echo
+echo "# after dns config"
+sudo nmcli c show
+echo
+echo
